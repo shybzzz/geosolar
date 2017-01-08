8b52532280e0dd2214c612dc7b404bf35236fddd
shybzzz <rostykrom@yahoo.com>
2017-01-08 07:19:19 +0000

    mat file is introduced

--- src/main/java/geosolar/springmvc/controller/FirstController.java
+++ src/main/java/geosolar/springmvc/controller/FirstController.java
@@ -1,17 +1,24 @@
 package geosolar.springmvc.controller;
 
+import com.jmatio.io.MatFileWriter;
+import com.jmatio.types.MLArray;
+import com.jmatio.types.MLDouble;
 import geosolar.springmvc.domain.Dst;
 import geosolar.springmvc.service.FirstService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 
 @Controller
 public class FirstController {
@@ -23,6 +30,22 @@ public class FirstController {
     @ResponseBody
     public List<Dst> getDstByHour(@PathVariable("hour") int hour) {
         return this.firstService.getDstByHour(hour);
+    }
+
+    @RequestMapping(value = "/dst/mat", method = RequestMethod.GET)
+    @ResponseBody
+    public void getFile(HttpServletResponse response) throws IOException {
+        double[][] d = new double[1][];
+        d[0] = new double[1];
+        d[0][0] = 1;
+        MLDouble d1 = new MLDouble("d", d);
+
+        ArrayList<MLArray> data = new ArrayList<>();
+        data.add(d1);
+        String fileName = "hallo.mat";
+        new MatFileWriter(fileName, data);
+        InputStream inputStream = new FileInputStream(fileName);
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
     }
 
 }
